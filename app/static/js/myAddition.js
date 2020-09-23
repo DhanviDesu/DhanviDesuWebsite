a84b539b54237efa7086c9301755945a884e8cfa
DDFTW <dhanvi@gmail.com>
2020-09-22 18:44:00 -0700

    new repo:

---
+++ app/static/js/myAddition.js
@@ -0,0 +1,24 @@
+// Tooltips Initialization
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+function copyEmail() {
+    var toCopy = document.createElement("input")
+
+    document.body.appendChild(toCopy)
+
+    toCopy.setAttribute('value', "ddesu@ucsd.edu")
+    toCopy.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(toCopy);
+
+    //trying to get tooltip text to change
+    // var tool = document.getElementById("email");
+    // tool.setAttribute('title', "Copied: ddesu@ucsd.edu");
+    // $(element).attr('title', 'NEW_TITLE')
+    //     .tooltip('fixTitle')
+    //     .tooltip('show');
+    // tool.style.backgroundColor = "green";
+}
