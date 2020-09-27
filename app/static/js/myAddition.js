a2b10b7164d48fd7fff3115069124ed66064b48f
DDFTW <dhanvi@gmail.com>
2020-09-27 14:00:26 -0700

    Final changes to structure

--- app/static/js/myAddition.js
+++ app/static/js/myAddition.js
@@ -4,21 +4,20 @@ $(function() {
 })
 
 function copyEmail() {
-    var toCopy = document.createElement("input")
+    var toCopy = document.createElement("input");
 
-    document.body.appendChild(toCopy)
+    document.body.appendChild(toCopy);
 
-    toCopy.setAttribute('value', "ddesu@ucsd.edu")
+    toCopy.setAttribute('value', "email@comp.com");
     toCopy.select();
 
     document.execCommand('copy');
     document.body.removeChild(toCopy);
 
     //trying to get tooltip text to change
-    // var tool = document.getElementById("email");
-    // tool.setAttribute('title', "Copied: ddesu@ucsd.edu");
-    // $(element).attr('title', 'NEW_TITLE')
-    //     .tooltip('fixTitle')
-    //     .tooltip('show');
-    // tool.style.backgroundColor = "green";
+    var tool = document.getElementById("email");
+    tool.setAttribute('title', "Copied: email");
+    $(element).attr('title', 'NEW_TITLE')
+        .tooltip('fixTitle')
+        .tooltip('show');
 }
